95e1072097b59c8cfcadd1a7e899e86572293cc3
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2026-03-06 15:25:16 +0530

    log removed

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const uri = process.env.MONGO_DB_URL;
-console.log("MongoDB URI:", uri);
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 async function ConnectToDatabase() {
